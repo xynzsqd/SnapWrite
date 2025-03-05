bd902650f60cfdeadd57c0e200e34289052deb7f
xynz <sqdcontrast@gmail.com>
2025-03-05 10:47:23 +0300

    feat(posts): add text-post component

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -3,29 +3,7 @@
     <ul class="flex flex-col justify-center items-center gap-8">
         @foreach (range(0, 4) as $post)
             <li class="w-2/3">
-                <div class="border p-8">
-                    <div class="flex gap-3 items-center mb-2">
-                        <div>
-                            <img class="w-16 h-16 object-cover rounded-full"
-                                src="https://images.unsplash.com/photo-1740124659051-71da6ea354e0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                alt="avatar">
-                        </div>
-                        <div class="flex flex-col">
-                            <a href="#" class="font-medium w-fit inline hover:underline underline-offset-4">xynz</a>
-                            <span class="text-gray-600">01.10.22 в 12:05</span>
-                        </div>
-                    </div>
-                    <div>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque
-                            temporibus
-                            architecto necessitatibus ducimus totam ipsa quasi deleniti iure provident
-                            veniam
-                            ex, laborum sed optio quod id quae minima illo. Inventore maxime earum odio
-                            neque
-                            est ullam. Quidem maxime at, optio sed, temporibus nobis amet fuga aliquid eius,
-                            corporis aut!</p>
-                    </div>
-                </div>
+                <x-posts.text-post />
             </li>
             <li class="w-2/3">
                 <div class="border py-8 px-4">
