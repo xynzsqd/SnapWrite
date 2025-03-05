bf597c4b376396f560a19dd64b024c904c1506d7
xynz <sqdcontrast@gmail.com>
2025-03-05 10:50:08 +0300

    feat(posts): add post-header component

---
+++ resources/views/components/posts/post-header.blade.php
@@ -0,0 +1,11 @@
+<div class="flex gap-3 items-center mb-2">
+    <div>
+        <img class="w-16 h-16 object-cover rounded-full"
+            src="https://images.unsplash.com/photo-1740124659051-71da6ea354e0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="avatar">
+    </div>
+    <div class="flex flex-col">
+        <a href="#" class="font-medium w-fit inline hover:underline underline-offset-4">xynz</a>
+        <span class="text-gray-600">01.10.22 в 12:05</span>
+    </div>
+</div>
