a255ebeea1d3704bc4ffdf54b34cb0be5c40944b
xynz <sqdcontrast@gmail.com>
2025-03-05 16:05:57 +0300

    feat(post): add class merge in post-header component

--- resources/views/components/posts/post-header.blade.php
+++ resources/views/components/posts/post-header.blade.php
@@ -1,4 +1,4 @@
-<div class="flex gap-3 items-center mb-2">
+<div {{$attributes->merge(['class' => 'flex gap-3 items-center'])}} >
     <div>
         <img class="w-16 h-16 object-cover rounded-full"
             src="https://images.unsplash.com/photo-1740124659051-71da6ea354e0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
