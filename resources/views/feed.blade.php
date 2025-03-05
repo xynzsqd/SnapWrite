c234aaa0e709dc4e3aa13754447e38d2c2565814
xynz <sqdcontrast@gmail.com>
2025-03-05 16:04:39 +0300

    feat(feed): add photo-post component in feed view

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -6,30 +6,8 @@
                 <x-posts.text-post />
             </li>
             <li class="w-2/3">
-                <div class="border py-8 px-4">
-                    <div class="flex gap-3 items-center mb-2">
-                        <div>
-                            <img class="w-16 h-16 object-cover rounded-full"
-                                src="https://images.unsplash.com/photo-1740124659051-71da6ea354e0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                alt="avatar">
-                        </div>
-                        <div class="flex flex-col">
-                            <a href="#" class="font-medium w-fit inline hover:underline underline-offset-4">xynz</a>
-                            <span class="text-gray-600">01.10.22 в 12:05</span>
-                        </div>
-                    </div>
-                    <a href="#">
-                        <div class="h-96 overflow-hidden relative mb-3 rounded-2xl">
-                            <img class="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
-                                src="https://images.unsplash.com/photo-1536589961747-e239b2abbec2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                alt="user's photo">
-                        </div>
-                    </a>
-                    <div class="px-4 flex justify-center gap-10 text-lg">
-                        <span>Likes: 34</span>
-                        <a href="#" class="hover:underline">Comments: 45</a>
-                    </div>
-                </div>
+                <x-posts.photo-post :isInFeed="true">
+                </x-posts.photo-post>
             </li>
         @endforeach
     </ul>
