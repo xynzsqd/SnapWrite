e3c8f82ff442a2d878bc2fdeefe888831b08b1d0
xynz <sqdcontrast@gmail.com>
2025-03-06 10:37:59 +0300

    feat(Users): configure factory for User model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,9 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -26,10 +28,17 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'avatar' => $this->generateAvatarUrl(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function generateAvatarUrl(): string
+    {
+        $id = mt_rand(1,100);
+        return "https://avatar.iran.liara.run/public/{$id}";
     }
 
     /**
