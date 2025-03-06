506f70f02b4713f2ff9b0ef11951be96058ee3b7
xynz <sqdcontrast@gmail.com>
2025-03-06 11:06:58 +0300

    feat(profile): set user's avatar in profile.index view

--- resources/views/components/profile/header.blade.php
+++ resources/views/components/profile/header.blade.php
@@ -5,7 +5,7 @@
       {{-- user's avatar --}}
       <div class="w-1/3 flex justify-center overflow-hidden rounded-full">
           <img class="w-36 h-36 object-cover rounded-full"
-              src="https://images.unsplash.com/photo-1740124659051-71da6ea354e0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src="{{$user->avatar}}"
               alt="user's avatar">
       </div>
 
